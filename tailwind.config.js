dd3b47d681e316beab2b00b71b2095cabb3a798f
mahyararzhangi <mahyararzhangi@gmail.com>
2024-08-21 15:06:01 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html", "./public/**/*.js", "./public/**/*.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
